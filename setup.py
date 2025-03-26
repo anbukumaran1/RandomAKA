6ab0b55d89c43d9b7282e31785e8154fd9be2947
Anbu kumaran A <148788136+anbukumaran1@users.noreply.github.com>
2025-03-26 18:38:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="RandomAK",  # Your package name
-    version="0.1.0",  # Start with an initial version
+    name="RandomAKA",  # Your package name
+    version="1.0.0",  # Start with an initial version
     author="AnbuKumaran Arangaperumal",  # Your name
     author_email="anbuku12345@gmail.com",  # Your email
     description="A Python package for enhanced randomization functions",  # Short description
     long_description=open("README.md").read(),  # Import README.md for long description
     long_description_content_type="text/markdown",  # Markdown format
-    url="https://github.com/anbukumaran1/RandomAK",  # Link to your GitHub repo
+    url="https://github.com/anbukumaran1/RandomAKA",  # Link to your GitHub repo
     packages=find_packages(),  # Automatically find package folders
     license="MIT",  # Updated to SPDX format
     classifiers=[
